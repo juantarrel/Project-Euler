0cbe6b382d16422220f8fe06065ed687da26ca26
Juan Tarrel <juantarrel@gmail.com>
2016-11-07 17:11:03 -0800

    Problem 3 solution

--- javascript/problem-3.js
+++ javascript/problem-3.js
@@ -1,43 +1,107 @@
 var problem3 = (function () {
-  "use strict";
-  var prime,
-      primesFactor;
-  
-  prime = function (n) {
-    if (n === 1) {
-      return 0;
-    }
-    for (let x=2; x<=n-1; x++) {
-      if (n % x === 0) {
-        return 0;
-      }
-    }
-      return n;
-  }; 
-  
-  primesFactor = function (n) {
-    var thePrimesFactor = [],
-        posiblePrime,
-        primeNum;
-    for (let x=1, y=2; x<=n; x=x+2) {
-      posiblePrime = x;
-      if (x === 1) {
-        posiblePrime = y;
-      } 
-      primeNum = prime(posiblePrime);
-      if (primeNum !== 0 && n % primeNum === 0) {
-        thePrimesFactor.push(primeNum);
-      }
-    }
-    console.log(thePrimesFactor);
-  };
-  
-  return {
-    isPrime: function (n) {
-      return prime(n);
-    },
-    thePrimesFactor: function () {
-      return primesFactor(n);
-    }
-  };
+    "use strict";
+    var prime,
+        primesFactor;
+
+    prime = function (n) {
+        if (n === 1) {
+            return 0;
+        }
+        for (let x=2; x<=n-1; x++) {
+            if (n % x === 0) {
+                return 0;
+            }
+        }
+        return n;
+    };
+
+    primesFactor = function (n) {
+        var thePrimesFactor = [],
+            posiblePrime,
+            primeNum,isIntegerDiv = 1,
+            divNum,
+            resultDivPrim;
+        for (let x=1, y=2; x<=n; x=x+2) {
+            posiblePrime = x;
+            if (x === 1) {
+                posiblePrime = y;
+                resultDivPrim = n;
+            }
+
+            primeNum = prime(posiblePrime);
+
+            while (isIntegerDiv !== 0 && primeNum !== 0) {
+                divNum = resultDivPrim / primeNum;
+                if (Number.isInteger(divNum)) {
+                    isIntegerDiv = 1;
+                    resultDivPrim = divNum;
+                    thePrimesFactor.push(primeNum);
+                    if (divNum === 1) {
+                        break;
+                    }
+                } else {
+                    isIntegerDiv = 0;
+                }
+
+            }
+
+            if(divNum === 1){
+                break;
+            }
+            isIntegerDiv = 1;
+        }
+        console.log(thePrimesFactor);
+    };
+
+    return {
+        isPrime: function (n) {
+            return prime(n);
+        },
+        thePrimesFactor: function (n) {
+            return primesFactor(n);
+        }
+    };
 })();
+problem3.thePrimesFactor(600851475143);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
